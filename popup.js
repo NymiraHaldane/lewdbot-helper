2b7b319feb65dd3763b19c35171cc4d4ee13c21c
NymiraHaldane <redwallerinside@gmail.com>
2019-04-10 19:39:44 -0700

    add ability to store multiple channels
    Add the ability to store and retreive multiple channels and reject duplicates

--- popup.js
+++ popup.js
@@ -21,37 +21,60 @@ try {
 
 		element = document.getElementById("channelName");
 
-	  element.addEventListener('keypress', getKeyPress);
+		element.addEventListener('keypress', getKeyPress);
 
-		chrome.storage.sync.get('channelName', function(result) {
+		chrome.storage.sync.get('Bot Test', function(result) {
 			if (result == undefined) {
 				return null;
 			} else {
-			  element.value = result.channelName[0];
+				element.value = result['Bot Test'];
 			};
 		});
 	});
 }
 catch(error) {
-  console.error(error);
+	console.error(error);
 };
 
 function saveChanges(textarea) {
-  // Get a value saved in the Channel field.
-  channelName = textarea.value;
+	// Get a value saved in the Channel field.
+	channelName = textarea.value;
 
-  if (!channelName) {
-    alert('Please enter a channel name.');
-    return;
-  };
+	if (!channelName) {
+		alert('Please enter a channel name.');
+		return;
+	};
 
 	getChannelId().then(function(result) {
-		if (result == undefined) {
-			alert('No channel with name ' + channelName + ' found.');
-		}
-	  chrome.storage.sync.set({channelName: [channelName, channelId]}, function() {
-	    alert('Channel ' + channelName + ' saved.');
-	  });
+		var data = {};
+
+		data[channelName] = channelId;
+
+		chrome.storage.sync.set(data, function() {
+			alert('Channel ' + channelName + ' saved.');
+		});
+
+		chrome.storage.sync.get('channelNames', function(result) {
+			var names = result.channelNames;
+			result.channelNames = [];
+			if (names == undefined) {
+				names = [channelName];
+			} else if (names.includes(channelName)) {
+				return;
+			} else {
+				names.push(channelName);
+			};
+			result.channelNames = names;
+			chrome.storage.sync.set({channelNames: result.channelNames}, function() {
+				alert('saved');
+			});
+		});
+	});
+};
+
+function getChannels() {
+	return new Promise((resolve, reject) => {
+
 	});
 };
 
@@ -59,19 +82,18 @@ function getChannelId(callback) {
 	return new Promise((resolve, reject) => {
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function() {
-		  if (this.readyState == 4 && this.status == 200) {
-		    var channelData = JSON.parse(this.responseText);
-		    channelData.result.forEach(function(update) {
-		    	if (update.message.chat.title == channelName) {
-		    		channelId = update.message.chat.id;
-		    		resolve(channelId);
-		    	};
-		    });
-		  };
-		  try {
-		  	callback.apply(channelId,[id]);
-			}
-			catch(error) {
+			if (this.readyState == 4 && this.status == 200) {
+				var channelData = JSON.parse(this.responseText);
+				channelData.result.forEach(function(update) {
+					if (update.message.chat.title.toLowerCase() == channelName.toLowerCase()) {
+						channelId = update.message.chat.id;
+						resolve(channelId);
+					};
+				});
+			};
+		  	try {
+		  		callback.apply(channelId,[id]);
+			} catch(error) {
 				console.error(error);
 			};
 		};
@@ -81,7 +103,7 @@ function getChannelId(callback) {
 };
 
 function getKeyPress(e){
-  if(e.keyCode == 13){
-     saveChanges(element);
-  };
+	if(e.keyCode == 13){
+		saveChanges(element);
+	};
 };
